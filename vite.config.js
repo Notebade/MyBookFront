0cc8176b1de6084895661bc8a27512048e552210
Mity <dimondi1010@gmail.com>
2024-12-10 16:22:46 +0300

    config minor change

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -15,4 +14,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    host: '0.0.0.0',  // Это позволит приложению слушать на всех интерфейсах
+    port: 5173,        // Порт, на котором будет работать ваше приложение
+    open: false,        // Опционально: откроет приложение в браузере при запуске
+  }
 })
